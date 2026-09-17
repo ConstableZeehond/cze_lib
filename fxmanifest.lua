f21a08f4f929e7410a04672f74abee51357fd19e
Alex <alexhos75@gmail.com>
2026-09-17 11:06:48 +0100

    feat: added fxmanifest.lua
    Added the manifest file so it will load in-game.

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5enhanced'
+
+name 'cze_lib'
+author 'ConstableZeehond'
+description 'Library module for common functions'
+version '0.0.1'
+repository 'https://github.com/ConstableZeehond/cze_lib'
+
+files {}
+
+shared_scripts {}
+
+server_scripts {}
+
+client_scripts {}
